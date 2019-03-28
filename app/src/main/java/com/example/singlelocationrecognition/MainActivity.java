8e30e93d088bc738dc1b50ddac1e0cba32f5812b
changdaniel116 <changdaniel116@yahoo.com>
2019-03-28 01:54:26 -0500

    Fixed TextView initialization and added distance and headingdifference displays

--- app/src/main/java/com/example/singlelocationrecognition/MainActivity.java
+++ app/src/main/java/com/example/singlelocationrecognition/MainActivity.java
@@ -40,19 +40,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView txt_heading;
 
     //For recognition. Input x and y.
-    private double locationlat = 37.7584928;
-    private double locationlon = -122.3914718;
+    private double locationlat = 37.7585111;
+    private double locationlon = -122.3914671;
 
-    private double userlon;
-    private double userlat;
-    private double userheadingdeg;
+    private double userlon = 0;
+    private double userlat = 0;
+    private double userheadingdeg = 0;
 
     private boolean recognized = false;
     TextView txt_recognized;
 
+    private boolean validdist = false;
+    TextView txt_validdist;
+
     //For bug testing
     private double distance;
+    TextView txt_distance;
     private float headingdifference;
+    TextView txt_headingdifference;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
@@ -90,6 +95,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         txt_heading = (TextView) findViewById(R.id.txt_heading);
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //Init view of all of the textviews
+        txt_distance = (TextView) findViewById(R.id.txt_distance);
+        txt_headingdifference = (TextView) findViewById(R.id.txt_headingdifference);
+        txt_recognized = (TextView) findViewById(R.id.txt_recognized);
 
         //Necesary for location retrieval
         if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
@@ -172,10 +182,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             userheadingdeg = event.values[0];
 
             txt_heading.setText("Heading: " + Double.toString(degree) + " degrees");
+            txt_headingdifference.setText("Heading Difference: " + (userheadingdeg - radToDegree(calcRadBearing(userlat,userlon,locationlat,locationlon))) + " Degrees");
 
             // Continuous checking for now, on any heading change.
             recognized = compareDistance() && compareHeading();
-            txt_recognized.setText(String.valueOf(recognized));
+            txt_recognized.setText("Recognized?: " + String.valueOf(recognized));
 
 
 
@@ -185,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // not in use
+
     }
 
     //For location
@@ -202,9 +213,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                             + "/"
                             + String.valueOf(location.getLongitude()));
 
+                    txt_distance.setText("Distance: "+radToFeet(calcRadDistance(userlat,userlon,locationlat,locationlon)) + "feet");
+
                     // Continuous checking for now, on any heading change.
                     recognized = compareDistance() && compareHeading();
-                    txt_recognized.setText(String.valueOf(recognized));
+                    txt_recognized.setText("Recognized?:" + String.valueOf(recognized));
                 }
 
 
@@ -214,14 +227,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     //Location request settings (accuracy and interval)
     private void buildLocationRequest() {
+
+        long mult = (long) Math.pow(10,-1000);
         locationRequest = new LocationRequest();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(3000);
-        locationRequest.setSmallestDisplacement(10);
+        locationRequest.setInterval(5);
+        locationRequest.setFastestInterval(3);
     }
 
-    private double calcraddistance(double lat1, double lon1, double lat2, double lon2){
+    private double calcRadDistance(double lat1, double lon1, double lat2, double lon2){
 
         double t1 = Math.sin(lat1) * Math.sin(lat2);
         double t2 = Math.cos(lat1) * Math.cos(lat2);
@@ -233,11 +247,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private double calcmeterdistance(double rad){
-        return(rad * 3437.74677 * 1.1508 * 1.6093470878864446 * 1000);
+    private double radToFeet(double rad){
+        return(rad * 3437.74677 * 1.1508 *  5.2800102998e+3);
     }
 
-    private double calcradbearing(double lat1, double lon1, double lat2, double lon2){
+    private double calcRadBearing(double lat1, double lon1, double lat2, double lon2){
 
         double t1 = Math.sin(lat1) * Math.sin(lat2);
         double t2 = Math.cos(lat1) * Math.cos(lat2);
@@ -267,24 +281,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private double calcdegbearing(double rad){
+    private double radToDegree(double rad){
 
         return rad * (180 / Math.PI);
 
     }
 
     private boolean compareHeading(){
-        double maxheadingdiffdeg = 30;
-        double degree = calcdegbearing(calcradbearing(userlat,userlon,locationlat, locationlon));
+        double maxheadingdiffdeg = 60;
+        double degree = radToDegree(calcRadBearing(userlat,userlon,locationlat, locationlon));
 
         return (Math.abs(userheadingdeg-degree) < maxheadingdiffdeg);
     }
 
     private boolean compareDistance(){
-        double maxdistance = .5;
-        double meter = calcmeterdistance(calcraddistance(userlat, userlon, locationlat, locationlon));
+        double maxdistance = 50;
+        double feet = radToFeet(calcRadDistance(userlat, userlon, locationlat, locationlon));
 
-        return meter < maxdistance;
+        return feet < maxdistance;
 
     }
 }
